d6a57a62e967c0dce8820ed8dc7f0d84b8930781
Love1ex1111 <gitlerlove76@gmail.com>
2024-06-09 17:19:58 +0300

    25% процентов рабочая версия

--- Дипломная работа.py
+++ Дипломная работа.py
@@ -9,15 +9,18 @@ def start(message):
     btn2 = types.KeyboardButton("See balance")
     markup.add(btn1, btn2)
     bot.send_message(message.chat.id,"Hello, I'm a clicker bot ", reply_markup=markup)
-@bot.message_handler(commands=['Want to Clik'])
+@bot.message_handler(func=lambda message: message.text == 'Want to Clik')
 def hearts(message):
+    keyboard_two = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard = types.InlineKeyboardMarkup(row_width=1)
-    keyboard_two = types.ReplyKeyboardMarkup(row_width=1)
     callback = types.InlineKeyboardButton(text="Money", callback_data="knopka")
-    end = types.KeyboardButton(text="End")
+    end = types.KeyboardButton("End")
     keyboard_two.add(end)
     keyboard.add(callback)
+    bot.send_message(chat_id=message.chat.id, text="Добро пожаловать в игру кликер. 1 клик = 1 доллару",
+                     reply_markup=keyboard_two)
     bot.send_message(chat_id=message.chat.id, text="Нажмите кнопку:", reply_markup=keyboard)
+
 @bot.callback_query_handler(func=lambda call: True)
 def callback_hearts(call):
     if call.data == 'knopka':
